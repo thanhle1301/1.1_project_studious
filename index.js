2589ed6b07f801fc7cec0c5e6074c103ca64055e
thanhle1301 <letatthanh1995@gmail.com>
2023-08-20 02:25:10 +0700

    responsive lan 3

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // Thêm mã JavaScript sau cùng trong tệp HTML hoặc liên kết từ một tệp .js riêng
 document.addEventListener("DOMContentLoaded", function () {
-    var headerContent = document.querySelector(".header__content");
-  
-    window.addEventListener("scroll", function () {
-      if (window.scrollY > headerContent.offsetHeight) {
-        headerContent.classList.add("scrolled");
-      } else {
-        headerContent.classList.remove("scrolled");
-      }
-    });
+  var headerContent = document.querySelector(".header__content");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > headerContent.offsetHeight) {
+      headerContent.classList.add("scrolled");
+    } else {
+      headerContent.classList.remove("scrolled");
+    }
   });
-  
+});
